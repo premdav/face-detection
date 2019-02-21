17674a02793c60a2b9541bca973fce43cb6907a2
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-21 14:48:27 -0600

    refactoring code

--- src/App.js
+++ src/App.js
@@ -28,23 +28,25 @@ const particlesOptions = {
   }
 };
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+};
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -98,6 +100,7 @@ class App extends Component {
          .then(count => {
            this.setState(Object.assign(this.state.user, { entries: count }));
          })
+         .catch(console.log);
        }
        this.displayFaceBox(this.calculateFaceLocation(response))
      })
@@ -106,7 +109,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if(route === 'signout') {
-      this.setState({ isSignedIn: false});
+      this.setState(initialState);
     } else if(route === 'home') {
       this.setState({ isSignedIn: true });
     }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className="center ma">
             <div className='absolute mt2'>
-                <img id='inputimage' src={imageUrl} alt='user provided' width='500px' height='auto' />
+                <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto' />
                 <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.botRow, left: box.leftCol}}></div>
             </div>
         </div>
